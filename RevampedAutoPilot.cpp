8781bf0dfaa0be020fc8afd804357d3e85e9c1af
Lanc33llis <scdelance@gmail.com>
2021-05-24 21:51:00 -0500

    latest

--- RevampedAutoPilot.cpp
+++ RevampedAutoPilot.cpp
@@ -69,18 +69,22 @@ int main() {
     //return 0;
 
     auto p1 = path[0], p2 = path[1];
-    Mat ctrl = { {p1.X, Angle2Deriv(p1.Angle), p2.X, Angle2Deriv(p2.Angle)} };
-    Mat conversion = Mat::InverseBeizerBasis() * Mat::HermiteBasis();
-    Mat bezierCtrl = conversion * ctrl;
+    auto
+        x0 = p1.X, x1 = p2.X,
+        y0 = p1.Y, y1 = p2.Y,
+        d0 = Angle2Deriv(p1.Angle), d1 = Angle2Deriv(p2.Angle);
+    auto nx1 = x0, nx2 = x0 + (double)1 / 3 * (x1 - x0), nx3 = x1 - (double)1 / 3 * (x1 - x0), nx4 = x1;
+    auto ny1 = y0, ny2 = y0 + (double)1 / 3 * d0 * (y1 - y0), ny3 = y1 + (double)1 / 3 * d1 * (y1 - y0), ny4 = y1;
+
+    Mat yctrl = { {ny1, ny2, ny3, ny4} };
+    Mat xctrl = { {nx1, nx2, nx3, nx4} };
+
     Mat t(1, 4, 1);
     t.setPowers({ {1}, {1}, {2}, {3} });
     t.setVariables({ {0}, {1}, {1}, {1} });
-    auto i = t.input(1);
+    auto i = t.input(.5);
 
-    auto result = i * Mat::BeizerBasis() * bezierCtrl;
-    result.print();
-    //bezierCtrl.print();
-    return 0;
+    auto x = i * Mat::BeizerBasis() * xctrl, y = i * Mat::BeizerBasis() * yctrl;
 }
 
 //maybe do a bit of automation for increased smoothness. get linear slope between two slopes and like use that instead of user input if not given
