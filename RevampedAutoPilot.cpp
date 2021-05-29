984c37181499350b574c6bc74dcdc09637901a6d
Lanc33llis <scdelance@gmail.com>
2021-05-29 13:50:00 -0500

    latest cpp

--- RevampedAutoPilot.cpp
+++ RevampedAutoPilot.cpp
@@ -68,6 +68,7 @@ int main() {
 
     //return 0;
 
+
     auto p1 = path[0], p2 = path[1];
     auto
         x0 = p1.X, x1 = p2.X,
@@ -76,8 +77,8 @@ int main() {
     auto nx1 = x0, nx2 = x0 + (double)1 / 3 * (x1 - x0), nx3 = x1 - (double)1 / 3 * (x1 - x0), nx4 = x1;
     auto ny1 = y0, ny2 = y0 + (double)1 / 3 * d0 * (y1 - y0), ny3 = y1 + (double)1 / 3 * d1 * (y1 - y0), ny4 = y1;
 
-    Mat yctrl = { {ny1, ny2, ny3, ny4} };
-    Mat xctrl = { {nx1, nx2, nx3, nx4} };
+    BezierCurve yctrl = { {ny1, ny2, ny3, ny4} };
+    BezierCurve xctrl = { {nx1, nx2, nx3, nx4} };
 
     Mat t(1, 4, 1);
     t.setPowers({ {1}, {1}, {2}, {3} });
@@ -85,6 +86,31 @@ int main() {
     auto i = t.input(.5);
 
     auto x = i * Mat::BeizerBasis() * xctrl, y = i * Mat::BeizerBasis() * yctrl;
+    x.print(); y.print();
+
+    //roots
+    auto
+        xv1 = 3 * (xctrl[0][1] - xctrl[0][0]),
+        xv2 = 3 * (xctrl[0][2] - xctrl[0][1]),
+        xv3 = 3 * (xctrl[0][3] - xctrl[0][2]),
+        ax = xv1 - 2 * xv2 + xv3,
+        bx = 2 * (xv2 - xv1),
+        cx = xv1,
+        xrt1 = (-bx + std::sqrt(bx * bx - 4 * ax * cx)) / 2 * ax,
+        xrt2 = (-bx - std::sqrt(bx * bx - 4 * ax * cx)) / 2 * ax;
+    auto
+        yv1 = 3 * (yctrl[0][1] - yctrl[0][0]),
+        yv2 = 3 * (yctrl[0][2] - yctrl[0][1]),
+        yv3 = 3 * (yctrl[0][3] - yctrl[0][2]),
+        ay = yv1 - 2 * yv2 + yv3,
+        by = 2 * (yv2 - yv1),
+        cy = yv1,
+        yrt1 = (-by - std::sqrt(by * by - 4 * ay * cy)) / 2 * ay,
+        yrt2 = (-by + std::sqrt(by * by - 4 * ay * cy)) / 2 * ay;
+
+    cout << xctrl.at(xrt1) << "\n";
+        cout << xctrl.at(xrt2) << "\n";
+
 }
 
 //maybe do a bit of automation for increased smoothness. get linear slope between two slopes and like use that instead of user input if not given
